bec8c00d15aa01fffcaf006e7451b4af6523156d
Wilhelm Buchmüller <buch.willi@googlemail.com>
2018-02-11 17:29:50 +0100

    fix SSIM

--- programming/SSIM/SSIM.py
+++ programming/SSIM/SSIM.py
@@ -11,17 +11,28 @@ def main():
 
 def pairwise_ssim():
     files = os.listdir(os.curdir)
-    files = filter(lamda x: x.endswith(".png"))
+    files = filter(lambda x: x.endswith(".png"),files)
     for im1 in files:
         for im2 in files:
             x = imread(im1)
-            y = imread(im2)
-            
+            y = imread(im2)            
             ssim = compare_ssim(x,y,multichannel=True)
-            print("SSIM between: ", str(im1), "and: ", str(im2))
+            print("SSIM between: ", str(im1), "and: ", str(im2), ssim)
 
 def single_channel_ssim():
     sim = dict()
+    files = os.listdir(os.curdir)
+    files = filter(lambda x: x.endswith(".png"),files)
+    for i in range(0,3):
+        #do for single channel and add results to dict
+        for im1 in files:
+        for im2 in files:
+            x = imread(im1)
+            y = imread(im2)
+            x = x[:,:,i]  
+            y = y[:,:,i]      
+            ssim = compare_ssim(x,y,multichannel=True)
+            print("SSIM between: ", str(im1), "and: ", str(im2), ssim)
 
 if __name__ == '__main__':
     main()
