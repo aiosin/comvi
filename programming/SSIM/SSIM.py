594caf05c5d445756e6e7e4a5646de9f1ad19f0a
Wilhelm Buchmüller <buch.willi@googlemail.com>
2018-02-22 23:21:55 +0100

    working dbscan clustering

--- programming/SSIM/SSIM.py
+++ programming/SSIM/SSIM.py
@@ -2,29 +2,49 @@ from skimage.measure import compare_ssim
 from skimage.io import imread
 from skimage.transform import resize
 from itertools import combinations
+from sklearn.metrics.pairwise import pairwise_distances
+import numpy as np
+from functools import partial
 
 from sklearn.cluster import DBSCAN
 import sys
 import os
+import pickle
 
 fformats = ('.png','.jpeg','.jpg')
 
 def main():
-	print("skimage SSIM test")
-	print("-----------------")
-	pairwise_ssim()
 	files = os.listdir(os.curdir)
-	files = filter(lambda x: x.endswith(fformats),files)
-	DBSCAN(metric=ddistance,algorithm='brute').fit(files)
-
-def ddistance(x,y):
-	x = imread(x)
-	y = imread(y)
+	files = list(filter(lambda x: x.endswith(fformats),files))
+	#files = np.array(files).reshape(-1,1)
+	print(np.shape(files))
+	dist_matrix  = pairwise_distances(np.arange(len(files)).reshape(-1, 1), metric=partial(ddistance, files),n_jobs=-1)
+	print(dist_matrix)
+	with open('dist_matr.bin',mode='wb') as f:
+		pickle.dump(dist_matrix,f)
+	db = DBSCAN(eps=0.05,min_samples=2,metric='precomputed',algorithm='brute').fit(dist_matrix)
+	print(vars(db))
+	labels = db.labels_
+	print(db.labels_)
+	cnum = len(set(labels)) - (1 if -1 in labels else 0)
+	
+	
+'''
+ddistance custom distance metric for dbscan density based clustering algorithm
+'''
+def ddistance(files,x,y):
+	im1 = files[int(x)]
+	im2 = files[int(y)]
+	print(im1,im2)
+	x = imread(im1)
+	y = imread(im2)
 	x = resize(x,(100,100))
 	y = resize(y,(100,100))
 	return compare_ssim(x,y,multichannel=True)
 
-
+'''
+pairwise_ssim: calculates pairwise SSIM distances between a set of images and writes the results to file
+'''
 def pairwise_ssim():
 	files = os.listdir(os.curdir)
 	files = filter(lambda x: x.endswith(fformats),files)
@@ -40,7 +60,8 @@ def pairwise_ssim():
 	sims = sorted(sims, key=lambda x: x[2])
 	print(sims)
 	with open("results",mode='w') as f:
-		f.write(sims)
+		for item in sims:
+			f.write(item)
 	return sims
 
 def single_channel_ssim():
