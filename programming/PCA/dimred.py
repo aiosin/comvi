6d57313c51ad11505773e315d15e67cc2a89b30c
Wilhelm Buchmüller <buch.willi@googlemail.com>
2018-02-23 20:12:30 +0100

    experimental PCA and dimensionality reduction 2/2
    working feature extraction for moments and histogram information, PCA working, clustering with kmeans is next
    squash commit 
    
    
    squash

--- programming/PCA/dimred.py
+++ programming/PCA/dimred.py
@@ -7,52 +7,96 @@ from skimage.exposure import histogram
 from  scipy.stats import skew, kurtosis, entropy, energy_distance
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-
-_fformats = ['.jpg', '.png', ',jpeg', '.tga']
+from skimage.transform import resize
+import os
+from pprint import pprint
+_fformats = tuple(['.jpg', '.png', ',jpeg', '.tga'])
 
 def readimages(path=None):
-    imdata = np.array()
+    #cannot create empty array with numpy
+    #workaround 
+    imdata = []
+    i=0
     if(path is None):
-       	files = os.listdir(os.curdir)
+       	files = os.listdir("/home/zython/comvi/programming/SSIM/jpg")
        	files = list(filter(lambda x: x.endswith(_fformats),files))
         for file in files:
             #calculating features to create feature vector
-            image = imread(file)
+            try:
+                image = imread(file)
+            except FileNotFoundError as e:
+                continue
+            i+=1
+            image = resize(image,(100,100))
             image_flat = imread(file,flatten=True)
+            image_flat = resize(image_flat,(100,100))
+
+            r_im = image[:,:,0]
+            g_im = image[:,:,1]
+            b_im = image[:,:,2]
 
             #moments
-            #TODO: skimage moments only work on flat arrays
-            moments = cv2.HuMoments(image_flat)
-            #mo = skimage.measure.moments(image)
-            print(type(moments))
+            #moments = cv2.HuMoments(image_flat)
+            r_mo = skimage.measure.moments(r_im).flatten()
+            g_mo = skimage.measure.moments(g_im).flatten()
+            b_mo = skimage.measure.moments(b_im).flatten()
 
             #color histogram features
-            hist = histogram(image,bins=16)
-            mean = np.average(hist)
-            vx = np.var(hist)
-            skw = skew(hist)
-            kurt = kurtosis(hist)
-            ent = entropy(hist)
-            fvec =np.concatenate(moments,hist,mean,vx,skw,kurt,ent)
+            r_hist = np.histogram(r_im,bins=16)[0]
+            g_hist = np.histogram(g_im,bins=16)[0]
+            b_hist = np.histogram(b_im,bins=16)[0]
+            r_mean = np.average(r_hist)
+            g_mean = np.average(g_hist)
+            b_mean = np.average(b_hist)
+            r_vx = np.var(r_hist)
+            g_vx = np.var(g_hist)
+            b_vx = np.var(b_hist)
+            r_skw = skew(r_hist)
+            g_skw = skew(g_hist)
+            b_skw = skew(b_hist)
+            r_kurt = kurtosis(r_hist)
+            g_kurt = kurtosis(g_hist)
+            b_kurt = kurtosis(b_hist)
+            r_ent = entropy(r_hist)
+            g_ent = entropy(g_hist)
+            b_ent = entropy(b_hist)
+            fvec =np.array((r_mo,g_mo,b_mo,r_hist,g_hist,b_hist,r_mean,
+                                g_mean, b_mean,r_vx,g_vx,b_vx,r_skw,g_skw,
+                                b_skw,r_kurt, g_kurt,b_kurt,r_ent,g_ent,b_ent)).ravel()
+            fvec = np.reshape(fvec,-1)
+            fvec = np.hstack(fvec)
             imdata.append(fvec)
-        print(imdata.shape)
+    print(i)
     return imdata
 
-
+#little lambda for 'tuplifying' of numpy arrays if needed
 tuplify_array = lambda x: tuple(map(tuple,x))
 
-
+'''
+method doPCA: returns 2d coordinates of dimensionality reduction on given data
+'''
 def doPCA(arr):
     scaler =  StandardScaler()
     scaler.fit(arr)
-    scaler.transform(arr)
-    PCA(n_components=2)
+    arr =scaler.transform(arr)
+    pca =PCA(n_components=2)
+    X = pca.fit_transform(arr)
+    return X
+    
+    
     
 
 
 
 def main():
-    pass
+    feature_array = readimages()
+    coords =doPCA(feature_array)
+    X = [i[0] for i in coords]
+    Y = [i[1] for i in coords]
+    print(coords)
+    print(X,Y)
+    plt.scatter(X,Y)
+    plt.show()
 
 if __name__ == '__main__':
     main()
