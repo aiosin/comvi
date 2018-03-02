df10a3095e2e19b452bf1ff03d19079175554304
Wilhelm Buchmüller <buch.willi@googlemail.com>
2018-03-02 23:00:55 +0100

    added tensorflow example for feature extraction
    also: updated  readme and reorganized some imports

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # comvi
 Comparative Visualization of Molecular Surfaces using Similarity-based Clustering
 
+# Dependencies:
+- python:
+    - everything was written in python 3.6
+    - not tested in  python<3.6 (or 2.7 for that matter)
+    - dependencies:
+        - numpy
+        - tensorflow
+        - sklearn
+        - skimage
+        - scipy
+        - cv2
+        - pickle (not sure if pickle in stdlib)
+        - matplotlib for 2d plot-viz
+        - (rest should be in stdlib)
+- comvi C++:
+    - not sure
+    - probably only vanilla c++11/14 (?) apart from:
+        - glfw
+        - glew
+        - glad
+        - glm
+        - std_image
+        - stdlib (duh)
+        - KHR (?)
+        - opengl in general
 
 # Folder Structure:
 - papers:
---
+++ programming/neural_networks/classify_image.py
@@ -0,0 +1,88 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os.path
+import re
+import sys
+import tarfile
+
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
+
+'''
+Source: https://www.tensorflow.org/tutorials/image_recognition
+Original Source Copyright 2015 The TensorFlow Authors. All Rights Reserved
+'''
+DATA_URL = 'http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz'
+
+def compute_class_vec(image,modeldir):
+	"""
+	classifies given image in the 1000 category I
+	Args:
+		image: Image file name.
+
+	Returns:
+		numpy array of shape (1000,)
+	"""
+    #fetch pretrained imagenet or it already fetched make sure everything is in order
+	maybe_download_and_extract(modeldir)
+	if not tf.gfile.Exists(image):
+		tf.logging.fatal('File does not exist %s', image)
+	image_data = tf.gfile.FastGFile(image, 'rb').read()
+
+	# Creates graph from saved GraphDef.
+	with tf.gfile.FastGFile(os.path.join(
+			modeldir, 'classify_image_graph_def.pb'), 'rb') as f:
+		graph_def = tf.GraphDef()
+		graph_def.ParseFromString(f.read())
+		_ = tf.import_graph_def(graph_def, name='')
+
+	with tf.Session() as sess:
+		# Some useful tensors:
+		# 'softmax:0': A tensor containing the normalized prediction across
+		#   1000 labels.
+		# 'pool_3:0': A tensor containing the next-to-last layer containing 2048
+		#   float description of the image.
+		# 'DecodeJpeg/contents:0': A tensor containing a string providing JPEG
+		#   encoding of the image.
+		# Runs the softmax tensor by feeding the image_data as input to the graph.
+		softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+		predictions = sess.run(softmax_tensor,
+													 {'DecodeJpeg/contents:0': image_data})
+		predictions = np.squeeze(predictions)
+		print('shout it from the mountain tops')
+		print(predictions)
+		print(predictions.shape)
+		print('aylmao')
+        return predictions
+
+
+def maybe_download_and_extract(modeldir):
+	"""Download and extract model tar file."""
+	dest_directory = modeldir
+	if not os.path.exists(dest_directory):
+		os.makedirs(dest_directory)
+	filename = DATA_URL.split('/')[-1]
+	filepath = os.path.join(dest_directory, filename)
+	if not os.path.exists(filepath):
+		def _progress(count, block_size, total_size):
+			sys.stdout.write('\r>> Downloading %s %.1f%%' % (
+					filename, float(count * block_size) / float(total_size) * 100.0))
+			sys.stdout.flush()
+		filepath, _ = urllib.request.urlretrieve(DATA_URL, filepath, _progress)
+		print()
+		statinfo = os.stat(filepath)
+		print('Successfully downloaded', filename, statinfo.st_size, 'bytes.')
+	tarfile.open(filepath, 'r:gz').extractall(dest_directory)
+
+
+def main(_):
+	modeldir = os.path.join(os.getcwd(),'imagenet')
+	image = os.path.join(os.getcwd(),'cropped_panda.jpg')
+	compute_class_vec(image,modeldir)
+
+if __name__ == '__main__':
+	tf.app.run(main=main, argv=[sys.argv[0]])
