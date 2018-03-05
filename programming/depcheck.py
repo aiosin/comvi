f8b6532b808b72f9b88e041bc917051f02e537c7
Wilhelm Buchmüller <buch.willi@googlemail.com>
2018-03-05 22:27:15 +0100

    feature extraction 2/? and dep. checker
    feature extraction 2/?

---
+++ programming/depcheck.py
@@ -0,0 +1,19 @@
+import numpy
+import matplotlib.pyplot
+import tensorflow
+import scipy.stats 
+import sklearn
+import skimage.measure
+import mahotas
+import cv2
+
+import pprint 
+import requests
+
+def main():
+	print('all dependencies met')
+
+if __name__ == '__main__':
+	main()
+
+
