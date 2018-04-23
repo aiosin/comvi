13097e3e20d4fc28060bd3dbd7a80e81f8adc3e6
Wilhelm Buchmüller <buch.willi@googlemail.com>
2018-04-23 11:37:00 +0200

    cleanup 1/?
    cleaning up dimred

--- programming/PCA/dimred.py
+++ programming/PCA/dimred.py
@@ -20,8 +20,7 @@ from sklearn.cluster import MeanShift
 from sklearn.mixture import GaussianMixture
 from sklearn.mixture import VBGMM
 from sklearn.mixture import BayesianGaussianMixture
-from sklearn.manifold import TSNE
-from sklearn.manifold import Isomap
+from sklearn.manifold import TSNE,Isomap,LocallyLinearEmbedding,SpectralEmbedding,MDS
 
 
 from scipy.stats import skew, kurtosis, entropy, energy_distance
@@ -59,60 +58,7 @@ def arr2vec(path=None):
 		#you need to check if the script is running in the right directory
 		path = os.getcwd()
 	for file in files:
-		#calculating features to create feature vector
-		try:
-			image = imread(file)
-		except FileNotFoundError as e:
-			continue
-		except Exception as e:
-			continue
-		i+=1
-		#IMPORTANT: 
-		image = resize(image,(128,128))
-		image_flat = imread(file,flatten=True)
-		#
-		image_flat = resize(image_flat,(128,128))
-
-		#separating channels using slice
-		r_im = image[:,:,0]
-		g_im = image[:,:,1]
-		b_im = image[:,:,2]
-
-		#moments
-		#moments = cv2.HuMoments(image_flat)
-		r_mo = skimage.measure.moments(r_im).flatten()
-		g_mo = skimage.measure.moments(g_im).flatten()
-		b_mo = skimage.measure.moments(b_im).flatten()
-
-		#color histogram features
-		r_hist = np.histogram(r_im,bins=8)[0]
-		g_hist = np.histogram(g_im,bins=8)[0]
-		b_hist = np.histogram(b_im,bins=8)[0]
-		r_mean = np.average(r_hist)
-		g_mean = np.average(g_hist)
-		b_mean = np.average(b_hist)
-		r_vx = np.var(r_hist)
-		g_vx = np.var(g_hist)
-		b_vx = np.var(b_hist)
-		# r_skw = skew(r_hist)
-		# g_skw = skew(g_hist)
-		# b_skw = skew(b_hist)
-		r_kurt = kurtosis(r_hist)
-		g_kurt = kurtosis(g_hist)
-		b_kurt = kurtosis(b_hist)
-		r_ent = entropy(r_hist)
-		g_ent = entropy(g_hist)
-		b_ent = entropy(b_hist)
-		r_haralick = textural_features(r_im)
-		g_haralick = textural_features(g_im)
-		b_haralick = textural_features(b_im)
-
-		#THOUGHT: there *has* to be a better way of doing this.
-		fvec =np.array((r_mo,g_mo,b_mo,r_hist,g_hist,b_hist,r_mean,
-							g_mean, b_mean,r_vx,g_vx,b_vx,
-							#r_skw,g_skw,b_skw,
-							r_kurt, g_kurt,b_kurt,r_ent,g_ent,b_ent,
-							r_haralick,g_haralick,b_haralick,)).ravel()
+		fvec = im2vec(file)
 		fvec = np.reshape(fvec,-1)
 		fvec = np.hstack(fvec)
 		imdata.append(fvec)
@@ -124,10 +70,10 @@ tuplify_array = lambda x: tuple(map(tuple,x))
 #routing for one image
 #for hopefully parallelising the image vector routine
 def im2vec(file):
-	print(str(file))
 	#calculating features to create feature vector
 	try:
 		image = imread(file)
+		print('read: '+str(file))
 	except FileNotFoundError as e:
 		return
 	#except Exception as e:
@@ -221,6 +167,14 @@ def doPCA(arr):
 	X = pca.fit_transform(arr)
 	return X
 
+def doLLE(arr):
+	scaler =  StandardScaler()
+	scaler.fit(arr)
+	arr =scaler.transform(arr)
+	lle = LocallyLinearEmbedding(n_components=2)
+	x =lle.fit_transform(arr)
+	return x
+
 def dokPCA(arr):
 	scaler =  StandardScaler()
 	scaler.fit(arr)
@@ -239,8 +193,6 @@ def doG(arr):
 	y = mix.sample()
 	return x
 
-def nldimred(arr):
-	pass 
 
 def do_tsne(arr):
 	scaler =  StandardScaler()
@@ -257,6 +209,14 @@ def do_3_tsne(arr):
 	snek = TSNE(n_components=3)
 	X = snek.fit_transform(arr)
 	return X
+
+def do_mds(arr):
+	scaler =  StandardScaler()
+	scaler.fit(arr)
+	arr =scaler.transform(arr)
+	mds = MDS(n_components=2)
+	x = mds.fit_transform(arr)
+	return x
 
 def do_isomap(arr):
 	scaler =  StandardScaler()
@@ -374,6 +334,23 @@ def shape_features(im,fourier=False):
 	return cv2.HuMoments(im)
 	
 
+def asyncim2vec(mode='complex',path=None):
+	feature_array= []
+	#set max_workers accordingly workers=None equals max amount of cores*2 (or 4?)
+	with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
+		files = list(filter(lambda x: x.endswith('.png'), os.listdir(os.getcwd())))
+		if mode is not None:
+			if(mode =='complex')
+				futures = [executor.submit(im2vec,file) for file in files]
+			if(mode =='simple')
+				futures = [executor.submit(simpleim2vec,file) for file in files]
+		for future in concurrent.futures.as_completed(futures):
+			try:
+				feature_array.append(future.result()) 
+			except Exception as e:
+				print(e)
+	return feature_array
+
 
 def main():
 
@@ -382,7 +359,7 @@ def main():
 	# step = timeit.default_timer()
 	# feature_array = [im2vec(item) for item in sorted(os.listdir(os.getcwd())) ]
 	feature_array= []
-	#set max_workers accordingly
+	#set max_workers accordingly workers=None equals max amount of cores*2 (or 4?)
 	with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
 		#yikes, horrible oneliner incomming
 		# futures = [executor.submit(im2vec,item) for item in list(filter(lamdba x : x.endswith('.png'), os.listdir(os.getcwd())))]
@@ -393,31 +370,91 @@ def main():
 				feature_array.append(future.result()) 
 			except Exception as e:
 				print(e)
+	#I'll take what are for-loops for 400 ale
+	bmw_feat = []
+	with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
+		#yikes, horrible oneliner incomming
+		# futures = [executor.submit(im2vec,item) for item in list(filter(lamdba x : x.endswith('.png'), os.listdir(os.getcwd())))]
+		files = list(filter(lambda x: x.endswith(('.png','.jpg')), [os.path.join('/home/zython/comvi/programming/datasets/bmw_subset/',item) for item in os.listdir('/home/zython/comvi/programming/datasets/bmw_subset')]))
+		futures = [executor.submit(im2vec,file) for file in files]
+		for future in concurrent.futures.as_completed(futures):
+			try:
+				bmw_feat.append(future.result()) 
+			except Exception as e:
+				print(e)
+	
+	flower_feat  = []
+	with concurrent.futures.ThreadPoolExecutor(max_workers=None) as executor:
+		#yikes, horrible oneliner incomming
+		# futures = [executor.submit(im2vec,item) for item in list(filter(lamdba x : x.endswith('.png'), os.listdir(os.getcwd())))]
+		files = list(filter(lambda x: x.endswith(('.png','.jpg')), [os.path.join('/home/zython/comvi/programming/datasets/flower_subset/',item) for item in os.listdir('/home/zython/comvi/programming/datasets/flower_subset')]))
+		futures = [executor.submit(im2vec,file) for file in files]
+		for future in concurrent.futures.as_completed(futures):
+			try:
+				flower_feat.append(future.result()) 
+			except Exception as e:
+				print(e)
+
 
 	#sort the feature array based on the file, so arr2vec and im2vec in parallel should be equal 
 	feature_array = sorted(feature_array, key= lambda x: x[1])
 	stop  = timeit.default_timer()
 	feature_array = [item[0] for item in feature_array if item[0] is not None ]
+	bmw_feat = [item[0] for item in bmw_feat if item[0] is not None ]
+	flower_feat = [item[0] for item in flower_feat if item[0] is not None ]
 
 	coords =doPCA(feature_array)
 	X = [i[0] for i in coords]
 	Y = [i[1] for i in coords]
+
+	bmwcoords =doPCA(bmw_feat)
+	bmwX = [i[0] for i in bmwcoords]
+	bmwY = [i[1] for i in bmwcoords]
+
+	flowercoords =doPCA(flower_feat)
+	flowerX = [i[0] for i in flowercoords]
+	flowerY = [i[1] for i in flowercoords]
+
 	k_coords = dokPCA(feature_array)
 	kX = [i[0] for i in k_coords]
 	kY = [i[1] for i in k_coords]
 
-	#m_coords = doG(feature_array)
-	#mX = [i[0] for i in m_coords]
-	#mY = [i[1] for i in m_coords]
+	flowerk_coords = dokPCA(flower_feat)
+	flowerkX = [i[0] for i in flowerk_coords]
+	flowerkY = [i[1] for i in flowerk_coords]
+	
+	bmwk_coords = dokPCA(bmw_feat)
+	bmwkX = [i[0] for i in bmwk_coords]
+	bmwkY = [i[1] for i in bmwk_coords]
 
 	t_coords = do_tsne(feature_array)
 	tX = [i[0] for i in t_coords]
 	tY = [i[1] for i in t_coords]
 
+	bmwt_coords = do_tsne(bmw_feat)
+	bmwtX = [i[0] for i in bmwt_coords]
+	bmwtY = [i[1] for i in bmwt_coords]
+	
+	flowert_coords = do_tsne(flower_feat)
+	flowertX = [i[0] for i in flowert_coords]
+	flowertY = [i[1] for i in flowert_coords]
+
+
 	t3_coords = do_3_tsne(feature_array)
 	t3X = [i[0] for i in t3_coords]
 	t3Y = [i[1] for i in t3_coords]
 	t3Z = [i[2] for i in t3_coords]
+
+	flowert3_coords = do_3_tsne(flower_feat)
+	flowert3X = [i[0] for i in flowert3_coords]
+	flowert3Y = [i[1] for i in flowert3_coords]
+	flowert3Z = [i[2] for i in flowert3_coords]
+	
+	bmwt3_coords = do_3_tsne(bmw_feat)
+	bmwt3X = [i[0] for i in bmwt3_coords]
+	bmwt3Y = [i[1] for i in bmwt3_coords]
+	bmwt3Z = [i[2] for i in bmwt3_coords]
+
 	scaler =  StandardScaler()
 	scaler.fit(feature_array)
 	scaled_feature_array =scaler.transform(feature_array)
@@ -430,18 +467,70 @@ def main():
 	iX = [i[0] for i in i_coords]
 	iY = [i[1] for i in i_coords]
 
+	floweri_coords = do_isomap(flower_feat)
+	floweriX = [i[0] for i in floweri_coords]
+	floweriY = [i[1] for i in floweri_coords]
+
+	bmwi_coords = do_isomap(bmw_feat)
+	bmwiX = [i[0] for i in bmwi_coords]
+	bmwiY = [i[1] for i in bmwi_coords]
+
+	l_coords = doLLE(feature_array)
+	lX = [i[0] for i in l_coords]
+	lY = [i[1] for i in l_coords]
+
+	bmwl_coords = doLLE(bmw_feat)
+	bmwlX = [i[0] for i in bmwl_coords]
+	bmwlY = [i[1] for i in bmwl_coords]
+
+	flowerl_coords = doLLE(flower_feat)
+	flowerlX = [i[0] for i in flowerl_coords]
+	flowerlY = [i[1] for i in flowerl_coords]
+
+	mds_coords = do_mds(feature_array)
+	mdsX = [i[0] for i in mds_coords]
+	mdsY = [i[1] for i in mds_coords]
+
+	bmwmds_coords = do_mds(bmw_feat)
+	bmwmdsX = [i[0] for i in bmwmds_coords]
+	bmwmdsY = [i[1] for i in bmwmds_coords]
+	
+	flowermds_coords = do_mds(flower_feat)
+	flowermdsX = [i[0] for i in flowermds_coords]
+	flowermdsY = [i[1] for i in flowermds_coords]
+	plt.scatter(mdsX,mdsY)
+	plt.scatter(bmwmdsX,bmwmdsY,c="g")
+	plt.scatter(flowermdsX,flowermdsY,c="r")
+	plt.title('mds')
+	plt.figure()
+
+
+	plt.scatter(lX,lY)
+	plt.scatter(bmwlX,bmwlY,c="g")
+	plt.scatter(flowerlX,flowerlY,c="r")
+	plt.title("lle")
+	plt.figure()
+
 	plt.scatter(iX,iY)
+	plt.scatter(bmwiX,bmwiY,c="g")
+	plt.scatter(floweriX,floweriY,c="r")
 	plt.title('isomap')
 	plt.figure()
 
 
 	plt.title('normal PCA')
 	plt.scatter(X,Y)
+	plt.scatter(bmwX,bmwY,c="r")
+	plt.scatter(flowerX,flowerY,c="g")
 	plt.figure()
 	plt.scatter(kX,kY)
+	plt.scatter(flowerkX,flowerkY,c="g")
+	plt.scatter(bmwkX,bmwkY,c="r")
 	plt.title('kernel PCA')
 	plt.figure()
 	plt.scatter(tX,tY)
+	plt.scatter(bmwtX,bmwtY,c='g')
+	plt.scatter(flowertX,flowertY,c='r')
 	plt.title('tsne')
 
 	fig = plt.figure()
@@ -449,6 +538,8 @@ def main():
 	plt.title('tsne')
 
 	ax.scatter(t3X,t3Y,t3Z)
+	ax.scatter(bmwt3X,bmwt3Y,bmwt3Z,c='g')
+	ax.scatter(flowert3X,flowert3Y,flowert3Z,c='r')
 	plt.show()
 
 if __name__ == '__main__':
